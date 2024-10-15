263d99d4689cf34fb09b6daef2151541b03c6a83
Oleh Andrus <Oleh.Andrus@startpagina.nl>
2024-10-15 17:43:54 +0300

    module06

---
+++ module06/mocked_users/main.py
@@ -0,0 +1,23 @@
+# from mocker.users.fake_user import get_mocked_user
+from mocker import get_mocked_user
+import argparse
+import json
+
+
+parser = argparse.ArgumentParser(description="User data generator")
+parser.add_argument("--filename", help="JSON file", required=True)
+parser.add_argument("--amount", required=True)
+
+arguments = parser.parse_args()
+# print(arguments)
+my_arg = vars(arguments)
+# print(my_arg)
+
+filename = my_arg.get("filename")
+amount = int(my_arg.get("amount"))
+
+with open(filename, "w") as file:
+    mocked_users = list()
+    for i in range(amount):
+        mocked_users.append(json.dumps(get_mocked_user()))
+    file.writelines(mocked_users)
